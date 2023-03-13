ea5e01bfe6a6bba5fb1ea1cd6fad71616fba8b63
GabrielCD08 <127786290+GabrielCD08@users.noreply.github.com>
2023-03-13 19:21:30 -0300

    Add files via upload
    Calculadora simples em C#.

---
+++ Estudos/Program.cs
@@ -0,0 +1,39 @@
+﻿//Calculadora em português
+//Início
+//Escreve e determina a variável a partir do que é digitado
+Console.WriteLine("Digite um número: ");
+float a = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a expressão utilizada: ");
+char signal = char.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro número: ");
+float b = float.Parse(Console.ReadLine());
+// Determina o resultado a partir da operação realizada
+double c = 0;
+if (signal == '+')
+{
+    c = a + b;
+}
+else if (signal == '-')
+{
+    c = a - b;
+}
+else if (signal == '*')
+{
+    c = a * b;
+}
+else if (signal == '/')
+{
+    c = a / b;
+}
+else if (signal == '^')
+{
+    c = Math.Pow(a, b);
+}
+else if (signal == '%')
+{
+    c = (a/100)*b;
+}
+//Escreve o resultado
+Console.WriteLine("O resultado da expressão é: " + c);
+//Final
+
