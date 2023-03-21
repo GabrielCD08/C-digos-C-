f9a7e6226aac312999061ca2031dd5d8c6afa77e
GabrielCD08 <127786290+GabrielCD08@users.noreply.github.com>
2023-03-20 23:25:21 -0300

    Add files via upload

---
+++ Calculadora de Trocos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConsoleKeyInfo keyInfo;
+        float result = 0;
+        bool final = false;
+        float finalresult = 0;
+        do
+        {
+            Console.WriteLine("Total a pagar: " + result);
+            Console.WriteLine("Aperte S para adicionar mais produtos ou qualquer outra tecla para finalizar");
+
+            keyInfo = Console.ReadKey(true);
+            if (keyInfo.KeyChar == 'S' || keyInfo.KeyChar == 's')
+            {
+                Console.WriteLine("Digite o valor do produto: ");
+                float a = float.Parse(Console.ReadLine());
+                result += a;
+            }
+            else
+            {
+                final = true;
+            }
+                
+        }
+        while (!final);
+        Console.WriteLine("Total a pagar: " + result);
+
+        Console.WriteLine("Valor pago pelo cliente: ");
+        float val = float.Parse(Console.ReadLine());
+        finalresult = val - result;
+        
+        if (finalresult >= 0)
+        {
+            Console.WriteLine("O troco vai ser igual a: R$" + finalresult);
+        }
+        else
+        {
+            Console.WriteLine("O cliente falta pagar R$" + (finalresult*-1));
+        }
+    }
+}
