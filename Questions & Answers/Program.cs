9737e16f1fa4b0501161563b476e9809fee43d43
GabrielCD08 <127786290+GabrielCD08@users.noreply.github.com>
2023-04-04 22:51:17 -0300

    Add files via upload
    Você pode adicionar infinitas informações ao console, e manter uma conversa a partir dessas informações. Não se esqueça de separar, caso exista mais de uma resposta para uma única pergunta, com "/" as opções de resposta. Aproveite!

---
+++ Questions & Answers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> Input = new List<string>();
+        List<List<string>> Output = new List<List<string>>();
+        Console.WriteLine("Olá, converse comigo!");
+        string UserInput;
+        while(true)
+        {
+            Begin:
+            Console.WriteLine("Digite algo: ");
+            UserInput = Console.ReadLine();
+            for (int i = 0; i < Input.Count; i++)
+            {
+                if (Input[i] == UserInput)
+                {
+                    Random random = new Random();
+                    Console.WriteLine(Output[i][random.Next(Output[i].Count)]);
+                    goto Begin;
+                }
+            }
+            Console.WriteLine("Eu não sei como responder, como deveria?");
+            string NewInput;
+            NewInput = Console.ReadLine();
+            if (NewInput != "")
+            {
+                List<string> NewAnswers = new List<string>();
+                string[] responses = NewInput.Split('/');
+                for(int i = 0; i < responses.Length; i++)
+                {
+                    NewAnswers.Add(responses[i]);
+                }
+                Input.Add(UserInput);
+                Output.Add(NewAnswers);
+                goto Begin;
+            }
+        }
+    }
+}
+
